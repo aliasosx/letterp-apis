212185f919d292b3da16e7785534c811c04816a3
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2019-01-08 16:07:24 +0700

    u

--- routers/reports.js
+++ routers/reports.js
@@ -19,7 +19,7 @@ module.exports = server => {
     });
     server.get('/reportbyfoodType', async (req, res, next) => {
         try {
-            let sql = "select ft.food_type_desc,ft.food_type_desc_la, count(*) count, sum(fd.price) total ,date(o.order_datetime) 'Date' from foodtypes ft , food fd , orders o , orderdetails od where o.id = od.orderId and od.foodId = fd.id and fd.foodtypeId  = ft.id and date(o.order_datetime) = date(now()) group by fd.foodtypeId order by count(*) desc";
+            let sql = "select ft.food_type_desc,ft.food_type_desc_la, count(*) count, sum(fd.price) total ,date(o.order_datetime) 'Date' from foodtypes ft , food fd , orders o , orderdetails od where o.id = od.orderId and od.foodId = fd.id and fd.foodtypeId  = ft.id and date(o.order_datetime) = date(now())  and o.statusId=2 group by fd.foodtypeId order by count(*) desc";
             const r = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(r => {
                 res.send(r);
                 next();
