f19e2cd2697a33172168ac74db457c36c49ad295
alisaosx <aliasosx@hotmail.com>
2019-01-09 16:48:29 +0700

    udpate report

--- routers/reports.js
+++ routers/reports.js
@@ -6,9 +6,9 @@ const config = require('../config');
 const sequelize = require('../db/connectionInitializer');
 
 module.exports = server => {
-    server.get('/reportbykitchens', async (req, res, next) => {
+    server.get('/reportbykitchens/:id', async (req, res, next) => {
         try {
-            let sql = "select kitchen_name, count(fd.food_name) as count,sum(fd.cost) cost,sum(fd.price) total ,date(o.order_datetime) 'Date'  from kitchens ks , food fd , orderdetails ot , orders o where fd.kitchenId = ks.id and ot.foodId = fd.id and o.id = ot.orderId and o.statusId = 2 and date(o.order_datetime) = date(now()) group by ks.id order by count(*) desc";
+            let sql = "select kitchen_name, count(fd.food_name) as count,sum(fd.cost) cost,sum(fd.price) total ,date(o.order_datetime) 'Date'  from kitchens ks , food fd , orderdetails ot , orders o where fd.kitchenId = ks.id and ot.foodId = fd.id and o.id = ot.orderId and o.statusId = 2 and date(o.order_datetime) = date('" + req.params.id + "') group by ks.id order by count(*) desc";
             const r = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(r => {
                 res.send(r);
                 next();
@@ -17,9 +17,9 @@ module.exports = server => {
             res.send(new errors.InternalError(err.message));
         }
     });
-    server.get('/reportbyfoodType', async (req, res, next) => {
+    server.get('/reportbyfoodType/:id', async (req, res, next) => {
         try {
-            let sql = "select ft.food_type_desc,ft.food_type_desc_la, count(*) count, sum(fd.cost) cost, sum(fd.price) total ,date(o.order_datetime) 'Date' from foodtypes ft , food fd , orders o , orderdetails od where o.id = od.orderId and od.foodId = fd.id and fd.foodtypeId  = ft.id and date(o.order_datetime) = date(now())  and o.statusId=2 group by fd.foodtypeId order by count(*) desc";
+            let sql = "select ft.food_type_desc,ft.food_type_desc_la, count(*) count, sum(fd.cost) cost, sum(fd.price) total ,date(o.order_datetime) 'Date' from foodtypes ft , food fd , orders o , orderdetails od where o.id = od.orderId and od.foodId = fd.id and fd.foodtypeId  = ft.id and date(o.order_datetime) = date('" + req.params.id + "')  and o.statusId=2 group by fd.foodtypeId order by count(*) desc";
             const r = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(r => {
                 res.send(r);
                 next();
@@ -30,7 +30,7 @@ module.exports = server => {
     });
     server.get('/reporttopfood/:id', async (req, res, next) => {
         try {
-            let sql = "select fd.food_name, count(*) as count,sum(fd.cost) cost, sum(fd.price) total from food fd , orders o , orderdetails ot where fd.id = ot.foodId and ot.orderId = o.id and o.statusId = 2 and date(o.order_datetime) = date(now()) group by  fd.id order by  count(*) desc limit " + req.params.id;
+            let sql = "select fd.food_name, count(*) as count,sum(fd.cost) cost, sum(fd.price) total from food fd , orders o , orderdetails ot where fd.id = ot.foodId and ot.orderId = o.id and o.statusId = 2 and date(o.order_datetime) = date('" + req.params.id + "') group by  fd.id order by  count(*) desc ";
             const r = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(r => {
                 res.send(r);
                 next();
