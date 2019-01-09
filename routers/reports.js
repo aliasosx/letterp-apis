dcd8ab348b63ff46ddfebc998ee24ce126d97001
alisaosx <aliasosx@hotmail.com>
2019-01-09 16:52:36 +0700

    update report

--- routers/reports.js
+++ routers/reports.js
@@ -8,7 +8,7 @@ const sequelize = require('../db/connectionInitializer');
 module.exports = server => {
     server.get('/reportbykitchens/:id', async (req, res, next) => {
         try {
-            let sql = "select kitchen_name, count(fd.food_name) as count,sum(fd.cost) cost,sum(fd.price) total ,date(o.order_datetime) 'Date'  from kitchens ks , food fd , orderdetails ot , orders o where fd.kitchenId = ks.id and ot.foodId = fd.id and o.id = ot.orderId and o.statusId = 2 and date(o.order_datetime) = date('" + req.params.id + "') group by ks.id order by count(*) desc";
+            let sql = "select kitchen_name, count(*) ,sum(cost),sum(total) from orderdetails ot , food fd , orders o , kitchens ks  where o.id = ot.orderId and fd.id = ot.foodId and o.statusId = 2 and ks.id = fd.kitchenId and date(o.order_datetime) = date('" + req.params.id + "') group by  kitchen_name by count(*) desc";
             const r = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(r => {
                 res.send(r);
                 next();
