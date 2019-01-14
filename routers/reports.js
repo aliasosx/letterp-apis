8f5ef8ac208031005cb2d119ba59f3456814df33
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2019-01-14 23:41:54 +0700

    update reports

--- routers/reports.js
+++ routers/reports.js
@@ -9,7 +9,7 @@ module.exports = server => {
     // user reports
     server.post('/reportbykitchens', async (req, res, next) => {
         try {
-            let sql = "select kitchen_name, count(*) count ,sum(cost) cost,sum(total)  total from orderdetails ot , food fd , orders o , kitchens ks  where o.id = ot.orderId and fd.id = ot.foodId and o.statusId = 2 and ks.id = fd.kitchenId and date(o.order_datetime) = date('" + req.body.dt + "') and fd.kitchenId='" + req.body.kitchen + "' group by  kitchen_name order by count(*) desc";
+            let sql = "select kitchen_name, sum(ot.quantity) count ,sum(fd.cost*ot.quantity) cost,sum(total) total from orderdetails ot , food fd , orders o , kitchens ks  where o.id = ot.orderId and fd.id = ot.foodId and o.statusId = 2 and ks.id = fd.kitchenId and date(o.order_datetime) = date('" + req.body.dt + "') and fd.kitchenId='" + req.body.kitchen + "' group by  kitchen_name order by count(*) desc";
             const r = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(r => {
                 res.send(r);
                 next();
@@ -20,7 +20,7 @@ module.exports = server => {
     });
     server.post('/reportbyfoodType', async (req, res, next) => {
         try {
-            let sql = "select ft.food_type_desc,ft.food_type_desc_la, count(*) count, sum(fd.cost) cost, sum(fd.price) total ,date(o.order_datetime) 'Date' from foodtypes ft , food fd , orders o , orderdetails od where o.id = od.orderId and od.foodId = fd.id and fd.foodtypeId  = ft.id and date(o.order_datetime) = date('" + req.body.dt + "') and fd.kitchenId='" + req.body.kitchen + "'  and o.statusId=2 group by fd.foodtypeId order by count(*) desc";
+            let sql = "select ft.food_type_desc,ft.food_type_desc_la, sum(od.quantity) count, sum(fd.cost*od.quantity) cost, sum(fd.price*od.quantity) total,date(o.order_datetime) 'Date' from foodtypes ft , food fd , orders o , orderdetails od where o.id = od.orderId and od.foodId = fd.id and fd.foodtypeId  = ft.id and date(o.order_datetime) = date('" + req.body.dt + "') and fd.kitchenId='" + req.body.kitchen + "'  and o.statusId=2 group by fd.foodtypeId order by count(*) desc";
             const r = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(r => {
                 res.send(r);
                 next();
@@ -31,7 +31,7 @@ module.exports = server => {
     });
     server.post('/reporttopfood', async (req, res, next) => {
         try {
-            let sql = "select ot.full_food_name food_name, count(*) as count,sum(fd.cost) cost, sum(fd.price) total from food fd , orders o , orderdetails ot where fd.id = ot.foodId and ot.orderId = o.id and o.statusId = 2 and date(o.order_datetime) = date('" + req.body.dt + "') and fd.kitchenId='" + req.body.kitchen + "' group by  fd.id order by  count(*) desc ";
+            let sql = "select ot.full_food_name food_name, ot.quantity as count,sum(fd.cost * ot.quantity) cost, sum(fd.price*ot.quantity) total from food fd , orders o , orderdetails ot where fd.id = ot.foodId and ot.orderId = o.id and o.statusId = 2 and date(o.order_datetime) = date('" + req.body.dt + "') and fd.kitchenId='" + req.body.kitchen + "' group by  fd.id order by  count(*) desc ";
             const r = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(r => {
                 res.send(r);
                 next();
@@ -43,7 +43,7 @@ module.exports = server => {
     // admin reports
     server.post('/adminreportbykitchens', async (req, res, next) => {
         try {
-            let sql = "select kitchen_name, count(*) count ,sum(cost) cost,sum(total)  total , sum(o.grandtotal) as grandtotal from orderdetails ot , food fd , orders o , kitchens ks  where o.id = ot.orderId and fd.id = ot.foodId and o.statusId = 2 and ks.id = fd.kitchenId and date(o.order_datetime) between date('" + req.body.dt + "') and date('" + req.body.edt + "') group by  kitchen_name order by count(*) desc";
+            let sql = "select kitchen_name, sum(ot.quantity) count ,sum(cost*ot.quantity) cost,sum(total) total , o.grandtotal as grandtotal from orderdetails ot , food fd , orders o , kitchens ks  where o.id = ot.orderId and fd.id = ot.foodId and o.statusId = 2 and ks.id = fd.kitchenId and date(o.order_datetime) between date('" + req.body.dt + "') and date('" + req.body.edt + "') group by  kitchen_name order by count desc";
             const r = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(r => {
                 res.send(r);
                 next();
@@ -54,7 +54,7 @@ module.exports = server => {
     });
     server.post('/adminreportbyfoodType', async (req, res, next) => {
         try {
-            let sql = "select ft.food_type_desc,ft.food_type_desc_la, count(*) count, sum(fd.cost) cost, sum(fd.price) total ,date(o.order_datetime) 'Date' from foodtypes ft , food fd , orders o , orderdetails od where o.id = od.orderId and od.foodId = fd.id and fd.foodtypeId  = ft.id and date(o.order_datetime) between date('" + req.body.dt + "') and date('" + req.body.edt + "')   and o.statusId=2 group by fd.foodtypeId order by count(*) desc";
+            let sql = "select ft.food_type_desc,ft.food_type_desc_la, sum(od.quantity) count, sum(fd.cost*od.quantity) cost, sum(fd.price*od.quantity) total ,date(o.order_datetime) 'Date' from foodtypes ft , food fd , orders o , orderdetails od where o.id = od.orderId and od.foodId = fd.id and fd.foodtypeId  = ft.id and date(o.order_datetime) between date('" + req.body.dt + "') and date('" + req.body.edt + "')   and o.statusId=2 group by fd.foodtypeId order by count desc";
             const r = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(r => {
                 res.send(r);
                 next();
@@ -65,7 +65,7 @@ module.exports = server => {
     });
     server.post('/adminreporttopfood', async (req, res, next) => {
         try {
-            let sql = "select ot.full_food_name food_name, count(*) as count,sum(fd.cost) cost, sum(fd.price) total from food fd , orders o , orderdetails ot where fd.id = ot.foodId and ot.orderId = o.id and o.statusId = 2 and date(o.order_datetime) between date('" + req.body.dt + "') and date('" + req.body.edt + "')  group by  fd.id order by  count(*) desc ";
+            let sql = "select ot.full_food_name food_name, ot.quantity as count,sum(fd.cost * ot.quantity) cost, sum(fd.price*ot.quantity) total from food fd , orders o , orderdetails ot where fd.id = ot.foodId and ot.orderId = o.id and o.statusId = 2 and date(o.order_datetime) between date('" + req.body.dt + "') and date('" + req.body.edt + "')  group by  fd.id order by  count desc ";
             const r = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(r => {
                 res.send(r);
                 next();
@@ -76,7 +76,7 @@ module.exports = server => {
     });
     server.post('/adminreportbyuser', async (req, res, next) => {
         try {
-            let sql = "select  u.username, count(*) count,sum(ot.total) as total from orders o, users u , orderdetails ot where o.userId = u.id and o.id = ot.orderId and date(o.order_datetime) between date('" + req.body.dt + "') and date('" + req.body.edt + "') and o.statusId=2 group by u.id  order by  count(*) desc ";
+            let sql = "select u.username, sum(ot.quantity) count,sum(ot.total) as total from orders o, users u , orderdetails ot where o.userId = u.id and o.id = ot.orderId and date(o.order_datetime) between date('" + req.body.dt + "') and date('" + req.body.edt + "') and o.statusId=2 group by u.id  order by  count(*) desc ";
             const r = await sequelize.query(sql, { type: sequelize.QueryTypes.SELECT }).then(r => {
                 res.send(r);
                 next();
