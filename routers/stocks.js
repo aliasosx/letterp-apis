1532c587353de4159b7921512d01f2acf9f94e0e
alisaosx <aliasosx@hotmail.com>
2019-01-20 00:28:30 +0700

    update db

--- routers/stocks.js
+++ routers/stocks.js
@@ -23,7 +23,7 @@ module.exports = server => {
     });
     server.get('/products', async (req, res, next) => {
         try {
-            const c = await sequelize.query("select * from food fd, products p, producttypes pt, vendors v , stocks st, units u , users us where fd.id = p.foodId and p.productTypeId = pt.id and p.vendorId = v.id and p.id = st.productId and u.id = p.unitId and us.id = p.userId", { type: sequelize.QueryTypes.SELECT }).then((resp) => {
+            const c = await sequelize.query("select p.id , p.productCode,fd.food_name, p.productName, pt.id producttypeId,pt.productTypeCode, p.minimumStock,st.quantity,st.currentQuantity, u.id as unitId, u.unitName , u.unitSign, fd.cost, fd.price, v.id vendorId , v.shopName,st.stockDate,st.expiryDate,us.username,us.id userId  from food fd, products p, producttypes pt, vendors v , stocks st, units u , users us where fd.id = p.foodId and p.productTypeId = pt.id and p.vendorId = v.id and p.id = st.productId and u.id = p.unitId and us.id = p.userId", { type: sequelize.QueryTypes.SELECT }).then((resp) => {
                 res.send(resp);
                 next();
             });
